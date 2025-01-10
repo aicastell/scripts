48b0374f584476306f0940fb660a9f499b2589e4
Angel Ivan Castell Rovira <al004140@gmail.com>
2025-01-10 19:54:42 +0100

    markdown-to-mp3 script updated

--- src/markdown-to-mp3.sh
+++ src/markdown-to-mp3.sh
@@ -9,7 +9,7 @@ else
     echo "MD_FILE: $MD_FILE"
 fi
 
-echo -ne "A partir de $MD_FILE.md voy a crear $MD_FILE.mp3. Estar de acuerdo? y/n "
+echo -ne "Input file: $MD_FILE.md. Output file: $MD_FILE.mp3. Do you agree? y/n "
 read AGREE
 
 if [ $AGREE != "y" ]; then
@@ -19,6 +19,12 @@ fi
 
 python3 -m venv ~/venv
 source ~/venv/bin/activate
-pandoc ${MD_FILE}.md -t plain --wrap=none -o ${MD_FILE}.txt
+# pandoc ${MD_FILE}.md -t plain --wrap=none -o ${MD_FILE}.txt
 cat ${MD_FILE}.txt | gtts-cli -l es - -o ${MD_FILE}.mp3
 deactivate
+
+# Increase speed
+mplayer -speed 1.1 -ao pcm:fast:file=${MD_FILE}.wav ${MD_FILE}.mp3
+rm -f ${MD_FILE}.mp3
+lame ${MD_FILE}.wav ${MD_FILE}.mp3
+rm -f ${MD_FILE}.wav
